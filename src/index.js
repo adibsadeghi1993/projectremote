eb96ba6edff360af70802f49d8abc47130dc1b47
adib sadeghi <adibsadeghi7272@gmail.com>
2022-01-23 21:15:44 +0330

    bootstrap was added

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import "../node_modules/bootstrap/dist/css/bootstrap.rtl.min.css"
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min"
 
 ReactDOM.render(<App />, document.getElementById("root"));
